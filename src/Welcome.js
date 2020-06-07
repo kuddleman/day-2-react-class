06099d6766f399b0925bd8f98200173ab2635917
Kuddleman <edl1155@gmail.com>
2020-06-07 08:16:35 -0700

    initial commit

---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Welcome extends React.Component {
+
+  render() {
+    return (
+      <div>Welcome { this.props.user }</div>
+    )
+  }
+}
+
+export default Welcome
